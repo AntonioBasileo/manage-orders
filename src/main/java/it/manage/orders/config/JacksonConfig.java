f7526e783ad58d85e248c49995d0ffa9cbbe1f67
antoniobasileo <antonio.basileo92@gmail.com>
2026-02-14 17:14:15 +0100

    modified jackson file name

--- src/main/java/it/manage/orders/config/JacksonConfig.java
+++ src/main/java/it/manage/orders/config/JacksonConfig.java
@@ -2,15 +2,13 @@ package it.manage.orders.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import it.auth.security.starter.config.JwtFilter;
-import it.auth.security.starter.config.SecurityConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class SpringContext {
+public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
