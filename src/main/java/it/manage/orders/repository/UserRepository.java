523555903b80e59152cc3041ab458c30d1f19bd4
antoniobasileo <antonio.basileo92@gmail.com>
2026-02-15 21:30:32 +0100

    updated user repository

---
+++ src/main/java/it/manage/orders/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package it.manage.orders.repository;
+
+import it.auth.security.starter.repository.UserRepositoryProvider;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends UserRepositoryProvider {
+}
