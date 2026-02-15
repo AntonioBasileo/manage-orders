d2614a27c188e26fcee8f048468aafd1f7f813e5
antoniobasileo <antonio.basileo92@gmail.com>
2026-02-15 22:05:26 +0100

    minor fix

--- src/main/java/it/manage/orders/entity/AppUser.java
+++ src/main/java/it/manage/orders/entity/AppUser.java
@@ -1,22 +1,19 @@
 package it.manage.orders.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
-
+@Entity
+@Getter
+@Setter
 public class AppUser extends it.auth.security.core.entity.AppUser {
 
     @JsonIgnore
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "appUser")
     private Set<Order> orders;
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
-    private Set<String> roles;
 }
